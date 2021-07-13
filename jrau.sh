647f626fbb67a5c194498d78b00f5310a9dfb0d3
daniertg <87335182+daniertg@users.noreply.github.com>
2021-07-14 02:20:41 +0700

    Update jrau.sh
    p

--- jrau.sh
+++ jrau.sh
@@ -258,30 +258,6 @@ echo "DROPBEAR_BANNER="/etc/issue.net"" >> /etc/default/dropbear
 apt-get -y install fail2ban
 service fail2ban restart
 
-# Instal DDOS Flate
-if [ -d '/usr/local/ddos' ]; then
-	echo; echo; echo "Please un-install the previous version first"
-	exit 0
-else
-	mkdir /usr/local/ddos
-fi
-clear
-echo; echo 'Installing DOS-Deflate 0.6'; echo
-echo; echo -n 'Downloading source files...'
-wget -q -O /usr/local/ddos/ddos.conf http://www.inetbase.com/scripts/ddos/ddos.conf
-echo -n '.'
-wget -q -O /usr/local/ddos/LICENSE http://www.inetbase.com/scripts/ddos/LICENSE
-echo -n '.'
-wget -q -O /usr/local/ddos/ignore.ip.list http://www.inetbase.com/scripts/ddos/ignore.ip.list
-echo -n '.'
-wget -q -O /usr/local/ddos/ddos.sh http://www.inetbase.com/scripts/ddos/ddos.sh
-chmod 0755 /usr/local/ddos/ddos.sh
-cp -s /usr/local/ddos/ddos.sh /usr/local/sbin/ddos
-echo '...done'
-echo -n 'Creating cron to run script every minute.....\(Default setting\)'
-/usr/local/ddos/ddos.sh --cron > /dev/null 2>&1
-echo '.....done'
-
 # download script
 cd /usr/bin
 wget -O menu "https://raw.githubusercontent.com/daniertg/jraussh/main/menu.sh"
