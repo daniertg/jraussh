28581824ba615bcfb6fbb08ba9ccc977948b295a
daniertg <87335182+daniertg@users.noreply.github.com>
2021-07-14 01:11:28 +0700

    Update menu.sh

--- menu.sh
+++ menu.sh
@@ -11,14 +11,14 @@ echo -e "* restart         : restart service dropbear, webmin"
 echo -e "                    squid3, OpenVPN dan SSH"
 echo -e "* add-tr          : membuat akun trojan"
 echo -e "* del-tr          : hapus akun trojan"
-echo -e "* cek-tr          : cek user login trojan
+echo -e "* cek-tr          : cek user login trojan"
 echo -e "* renew-tr        : perpanjang akun trojan"
 echo -e "* add-ws          : membuat akun v2ray"
 echo -e "* del-ws          : hapus akun v2ray"
-echo -e "* cek-ws          : cek user login v2ray
+echo -e "* cek-ws          : cek user login v2ray"
 echo -e "* renew-ws        : perpanjang akun v2ray"
 echo -e "* reboot          : reboot VPS"
 echo -e "* speedtest       : speedtest VPS"
-echo -e "* info            : menampilkan informasi sistem
-echo -e "* exit            : keluar dari Putty
+echo -e "* info            : menampilkan informasi sistem"
+echo -e "* exit            : keluar dari Putty"
 echo -e " CREATOR BY FEBRIAN DANI RITONGA"
