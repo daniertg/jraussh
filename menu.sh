b3531de683263f383deba049ea3134c60b41afa1
daniertg <87335182+daniertg@users.noreply.github.com>
2021-07-13 21:29:41 +0700

    Create menu.sh

---
+++ menu.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+#Menu
+
+echo -e "* menu            : menampilkan daftar perintah"
+echo -e "* usernew         : membuat akun SSH & OpenVPN"
+echo -e "* trial           : membuat akun trial"
+echo -e "* hapus           : menghapus akun SSH & OpenVPN"
+echo -e "* cek             : cek user login"
+echo -e "* member          : daftar member SSH & OpenVPN"
+echo -e "* restart         : restart service dropbear, webmin"
+echo -e "                    squid3, OpenVPN dan SSH"
+echo -e "* add-trojan      : membuat akun trojan"
+echo -e "* hapus-trojan    : hapus akun trojan"
+echo -e "* renew-trojan    : perpanjang akun trojan"
+echo -e "* add-v2ray       : membuat akun v2ray"
+echo -e "* hapus-v2ray     : hapus akun v2ray"
+echo -e "* renew-v2ray     : perpanjang akun v2ray"
+echo -e "* reboot          : reboot VPS"
+echo -e "* speedtest       : speedtest VPS"
+echo -e "* info            : menampilkan informasi sistem
+echo -e "* exit            : keluar dari Putty/Connecbot/"
+echo -e "              JuiceSSH"
+echo -e " CREATOR BY FEBRIAN DANI RITONGA"
