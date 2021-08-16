24e13a44aba2d10bca70983deb1c805e1fb415e4
daniertg <87335182+daniertg@users.noreply.github.com>
2021-08-16 22:30:28 +0700

    Update fix2.sh

--- fix2.sh
+++ fix2.sh
@@ -93,8 +93,8 @@ echo "========================================================="
 # install dropbear
 apt-get -y install dropbear
 sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
-sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=44/g' /etc/default/dropbear
-sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 77 "/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=143/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 109"/g' /etc/default/dropbear
 echo "/bin/false" >> /etc/shells
 echo "/usr/sbin/nologin" >> /etc/shells
 /etc/init.d/ssh restart
@@ -140,15 +140,14 @@ client = no
 socket = a:SO_REUSEADDR=1
 socket = l:TCP_NODELAY=1
 socket = r:TCP_NODELAY=1
+
 [dropbear]
 accept = 222
-connect = 127.0.0.1:22
-[dropbear]
-accept = 444
-connect = 127.0.0.1:44
+connect = 127.0.0.1:143
+
 [dropbear]
 accept = 777
-connect = 127.0.0.1:77
+connect = 127.0.0.1:109
 END
 
 echo "=================  membuat Sertifikat OpenSSL ======================"
